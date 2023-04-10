b471d6872e1effef219355a748de7e21927e359d
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2023-04-10 15:20:09 +0300

    hw2

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+// const button = document.querySelector(".check");
+// const input = document.querySelector(".input");
+// const output = document.querySelector(".output");
+
+// button.addEventListener("click", function () {
+//   const sumOfAllDigits = [...input.value]
+//     .filter((ele) => ele >= 0 && ele <= 9)
+//     .reduce((a, b) => +a + +b);
+//   output.innerHTML = sumOfAllDigits;
+// });
+
 const button = document.querySelector(".check");
-const input = document.querySelector(".input");
+const input1 = document.querySelector(".input1");
+const input2 = document.querySelector(".input2");
 const output = document.querySelector(".output");
 
 button.addEventListener("click", function () {
-  const sumOfAllDigits = [...input.value]
-    .filter((ele) => ele >= 0 && ele <= 9)
-    .reduce((a, b) => +a + +b);
-  output.innerHTML = sumOfAllDigits;
+  const string = input1.value.toLowerCase();
+  const char = input2.value.toLowerCase();
+  const charNumOnString = [...string].filter((ele) => ele === char).length;
+
+  output.innerHTML = charNumOnString;
 });
